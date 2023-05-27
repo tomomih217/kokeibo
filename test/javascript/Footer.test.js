80ff73d0e523bac416a3cf37cf4db5c879ce01aa
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-05-27 22:39:53 +0900

    Add: Footerコンポーネントのテストコードを作成しました。

---
+++ test/javascript/Footer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from '../../app/javascript/components/Footer';
+import { toBeInTheDocument } from '@testing-library/jest-dom';
+
+
+describe('Footer', () => {
+  test('renders footer links correctly', () => {
+    // レンダリング
+    const { getByText } = render(<Footer />);
+
+    // リンクテキストを検証
+    expect(getByText('プライバシーポリシー')).toBeInTheDocument();
+    expect(getByText('利用規約')).toBeInTheDocument();
+    expect(getByText('お問い合わせ')).toBeInTheDocument();
+  });
+});
