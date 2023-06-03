2b81e5690007c616a1f11265fecaebf17d92fb43
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-04 00:45:44 +0900

    Add: SchoolButtonsコンポーネントのテストを作成しました。

--- app/javascript/components/features/SchoolButtons.js
+++ app/javascript/components/features/SchoolButtons.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '../components/Button';
 
 const SchoolButtons = ({ identifier, selectedButton, onChange }) => {
