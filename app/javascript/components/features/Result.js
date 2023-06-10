c5c0d0897600e517f51e1a69f7744d11d99129c2
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-10 23:21:11 +0900

    Add: シュミレーション結果のグラフを棒グラフと折れ線グラフの複合グラフに変更しました。

--- app/javascript/components/features/Result.js
+++ app/javascript/components/features/Result.js
@@ -4,47 +4,65 @@ import WithoutTitleCard from '../components/WithoutTitleCard';
 import Text from '../components/Text';
 import ResultGraph from './ResultGraph';
 
-const Result = ({ age, nurserySchool, kindergarten, primarySchool, juniorHighSchool, highSchool, university, livingAloneFunds }) => {
+const Result = ({ props }) => {
   //積立期間
-  const duration = (18 - age) * 12
+  const duration = (18 - props.age) * 12
 
   //積立総額
+  let costArray = [];
   const nurserySchoolCost =
-    nurserySchool === 'public' ? json.nurserySchool.public : json.nurserySchool.private;
+    props.nurserySchool === 'public' 
+      ? json.nurserySchool.public.age1 + json.nurserySchool.public.age2 
+      : json.nurserySchool.private.age1 + json.nurserySchool.private.age2;
+  costArray.push(nurserySchoolCost);
 
   const kindergartenCost =
-    kindergarten === 'public' ? json.kindergarten.public : json.kindergarten.private;
+    props.kindergarten === 'public' 
+      ? json.kindergarten.public.age3 + json.kindergarten.public.age4 + json.kindergarten.public.age5
+      : json.kindergarten.private.age3 + json.kindergarten.private.age4 + json.kindergarten.private.age5;
+  costArray.push(kindergartenCost);
 
   const primarySchoolCost =
-    primarySchool === 'public' ? json.primarySchool.public : json.primarySchool.private;
+    props.primarySchool === 'public'
+      ? json.primarySchool.public.age6 + json.primarySchool.public.age7 + json.primarySchool.public.age8 + json.primarySchool.public.age9 + json.primarySchool.public.age10 + json.primarySchool.public.age11
+      : json.primarySchool.private.age6 + json.primarySchool.private.age7 + json.primarySchool.private.age8 + json.primarySchool.private.age9 + json.primarySchool.private.age10 + json.primarySchool.private.age11;
+  costArray.push(primarySchoolCost);
 
   const juniorHighSchoolCost =
-    juniorHighSchool === 'public' ? json.juniorHighSchool.public : json.juniorHighSchool.private;
+    props.juniorHighSchool === 'public'
+      ? json.juniorHighSchool.public.age12 + json.juniorHighSchool.public.age13 + json.juniorHighSchool.public.age14
+      : json.juniorHighSchool.private.age12 + json.juniorHighSchool.private.age13 + json.juniorHighSchool.private.age14;
+  costArray.push(juniorHighSchoolCost);
 
   const highSchoolCost =
-    highSchool == 'public' ? json.highSchool.public : json.highSchool.private;
+    props.highSchool == 'public'
+      ? json.highSchool.public.age15 + json.highSchool.public.age16 + json.highSchool.public.age17
+      : json.highSchool.private.age15 + json.highSchool.private.age16 + json.highSchool.private.age17;
+  costArray.push(highSchoolCost);
 
-  let universityCost;
-  if ( university === 'publicArts' ) {
-    universityCost = json.university.publicArts;
-  } else if ( university === 'publicScience' ) {
-    universityCost = json.university.publicScience;
-  } else if ( university === 'privateArts' ) {
-    universityCost = json.university.privateArts;
-  } else if ( university === 'privateScience' ) {
-    universityCost = json.university.privateScience;
-  } else {
-    universityCost = 0;
+  let universityCost = 0;
+  if ( props.university === 'publicArts' ) {
+    universityCost = json.university.publicArts.age18 + json.university.publicArts.age19 + json.university.publicArts.age20 + json.university.publicArts.age21;
+  } else if ( props.university === 'publicScience' ) {
+    universityCost = json.university.publicScience.age18 + json.university.publicScience.age19 + json.university.publicScience.age20 + json.university.publicScience.age21;
+  } else if ( props.university === 'privateArts' ) {
+    universityCost = json.university.privateArts.age18 + json.university.privateArts.age19 + json.university.privateArts.age20 + json.university.privateArts.age21;
+  } else if ( props.university === 'privateScience' ) {
+    universityCost = json.university.privateScience.age18 + json.university.privateScience.age19 + json.university.privateScience.age20 + json.university.privateScience.age21;
   }
+  costArray.push(universityCost);
 
   let livingAloneCost;
-  if (livingAloneFunds > 0) {
+  if (props.livingAloneFunds > 0) {
     livingAloneCost = json.livingAllowance.initialize + livingAloneFunds * 12 * 4;
   } else {
     livingAloneCost = 0;
   }
+  costArray.push(livingAloneCost);
 
-  const amount = nurserySchoolCost + kindergartenCost + primarySchoolCost + juniorHighSchoolCost + highSchoolCost + universityCost + livingAloneCost;
+  const amount = costArray.reduce(function(a,b){
+    return a + b;
+  })
   const formattedAmount = amount.toLocaleString();
 
   //月々の積立金額
@@ -55,22 +73,22 @@ const Result = ({ age, nurserySchool, kindergarten, primarySchool, juniorHighSch
     <div>
       <WithoutTitleCard widthSize='1/2' bgColor='blue-100' borderColor='blue-500'>
         <div>
-          <Text size='lg' color='amber-dark' style='mb-3'>{age}〜18才まで　{duration}ヶ月</Text>
+          <Text size='lg' color='amber-dark' style='mb-3'>{props.age}〜18才まで　{duration}ヶ月</Text>
           <Text size='3xl' color='amber-dark' style='mb-3'>総額： {formattedAmount}円</Text>
           <Text size='lg' color='amber-dark'>月額　約{formattedMonthlyAmount}円</Text>
         </div>
       </WithoutTitleCard>
 
       <div className='w-1/2 m-auto'>
-        <ResultGraph monthlyAmount={monthlyAmount} />
+        <ResultGraph monthlyAmount={monthlyAmount} costArray={costArray} />
       </div>
 
       <div className='mt-3'>
         <Text size='xs' color='amber-dark'>
-          『平成30年度子供の学習費調査の結果について（文部科学省より・令和元年１２月発行）』
+          『令和3年度子供の学習費調査の結果について（文部科学省より・令和4年12月発行）』
         </Text>
         <Text size='xs' color='amber-dark'>
-          『令和３年度「教育費負担の実態調査結果」（日本政策金融公庫より・令和３年１２月発行）』
+          『令和3年度「教育費負担の実態調査結果」（日本政策金融公庫より・令和3年12月発行）』
         </Text>
       </div>
 
--- app/javascript/components/features/ResultGraph.js
+++ app/javascript/components/features/ResultGraph.js
@@ -4,6 +4,10 @@ import {
     CategoryScale,
     LinearScale,
     BarElement,
+    BarController,
+    PointElement,
+    LineElement,
+    LineController,
     Title,
     Tooltip,
     Legend,
@@ -14,32 +18,69 @@ ChartJS.register(
   CategoryScale,
   LinearScale,
   BarElement,
+  LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  LineController,
+  BarController,
+  PointElement
 );
 
-const ResultGraph = ({ monthlyAmount }) => {
-  let datas = [0];
+const ResultGraph = ({ monthlyAmount, costArray }) => {
+  let savingDatas = [0];
   let current = 0;
   for (let i = 0; i < 6; i++) {
     const ans = current + monthlyAmount * 36;
-    datas.push(ans);
+    savingDatas.push(ans);
     current = ans;
   }
+  savingDatas.push(current);
+
+  let costDatas = [0];
+  let costData = 0;
+  //3才までの累計コスト
+  costData = costArray[0];
+  costDatas.push(costData);
+  //6才までの累計コスト
+  costData = costData + costArray[1]
+  costDatas.push(costData);
+  //９才までの累計コスト
+  costData = costData + costArray[2] / 2
+  costDatas.push(costData);
+  //12才までの累計コスト
+  costData = costData + costArray[2] / 2
+  costDatas.push(costData);
+  //15才までの累計コスト
+  costData = costData + costArray[3]
+  costDatas.push(costData);
+  //18才までの累計コスト
+  costData = costData + costArray[4]
+  costDatas.push(costData);
+  //22才までの累計コスト
+  costData = costData + costArray[5]
+  costDatas.push(costData);
+
+
 
   const graphData = {
     labels: [
       //軸ラベル
-      '0才', '3才', '6才', '9才', '12才', '15才', '18才'
+      '0才', '3才', '6才', '9才', '12才', '15才', '18才', '22才'
     ],
     datasets: [
       //表示するデータセット
       {
-        data: datas,
+        type: 'bar',
+        data: savingDatas,
         backgroundColor: 'rgba(30, 144, 255, 0.5)',
         label: '積立累計額（円）',
       },
+      {
+        type: 'line',
+        data: costDatas,
+        label: '教育費（円）',
+      }
     ],
   };
 
