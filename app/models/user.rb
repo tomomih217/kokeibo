b83080d27b5fd0c94dde50074e873299327ceb66
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-20 01:06:04 +0900

    Add: Userモデルにバリデーションを追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :username, presence: true, length: { maximum: 255 }
+  # 新規登録かパスワード変更の時に以下のバリデーションを適用する。
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
