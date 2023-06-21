e9a8d0bdb0bd434f567fc33bae6b1ccb11b10a07
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-22 01:02:52 +0900

    Add: RSpecのフォーカス設定やwebdriverの設定を行いました。

---
+++ spec/support/config/driven_setting.rb
@@ -0,0 +1,7 @@
+Rspec.configure do |config|
+  config.before(:each, type: :system) do
+    # driven_by(:rack_test)
+    # driven_by(:selenium_chrome)
+    driven_by(:selenium_chrome_headless)
+  end
+end
