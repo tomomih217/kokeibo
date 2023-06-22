c6e967362313a04a74bed80680207a2f721b803f
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-22 10:32:28 +0900

    Add: FactoryBotでUserデータを作成しました。rspec-railsを4.0.1にアップグレードしました。

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 3.7'
+  gem 'rspec-rails', '~> 4.0.1'
   gem 'factory_bot_rails', '~> 4.11'
 end
 
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe 'before log in,' do
+    describe 'Sign in' do
+      fcontext 'with all attributes' do
+        before do
+          visit new_user_path
+          fill_in 'ユーザー名', with: 'Test_1'
+          fill_in 'パスワード', with: 'password'
+          fill_in 'パスワード（確認用）', with: 'password'
+          check '利用規約とプライバシーポリシーに同意する'
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+          click_button '確認画面へ'
+
+          click_button '登録する'
+        end
+        it 'is successful' do
+          expect(current_path).to eq users_complete_path
+          expect(page).to have_content '会員登録が完了しました'
+        end
+      end
+    end
+  end
 end
