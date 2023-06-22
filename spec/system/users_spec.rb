191f61c926493423513e4975f1383952d4a3d804
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-22 10:48:55 +0900

    Add: RSpecでユーザー新規登録機能のテストを作成しました。

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :system do
   describe 'before log in,' do
     describe 'Sign in' do
-      fcontext 'with all attributes' do
+      context 'with all attributes' do
         before do
           visit new_user_path
           fill_in 'ユーザー名', with: 'Test_1'
@@ -18,6 +18,37 @@ RSpec.describe "Users", type: :system do
         it 'is successful' do
           expect(current_path).to eq users_complete_path
           expect(page).to have_content '会員登録が完了しました'
+        end
+      end
+
+      context 'without name' do
+        before do
+          visit new_user_path
+          # nameの入力をしない
+          fill_in 'パスワード', with: 'password'
+          fill_in 'パスワード（確認用）', with: 'password'
+          check '利用規約とプライバシーポリシーに同意する'
+
+          click_button '確認画面へ'
+        end
+        it 'is failed' do
+          expect(page).to have_content 'Nameを入力してください'
+        end
+      end
+
+      context 'with duplicated name' do
+        let!(:duplicated_user){ create(:user, name: 'Test_1') }
+        before do
+          visit new_user_path
+          fill_in 'ユーザー名', with: 'Test_1'
+          fill_in 'パスワード', with: 'password'
+          fill_in 'パスワード（確認用）', with: 'password'
+          check '利用規約とプライバシーポリシーに同意する'
+
+          click_button '確認画面へ'
+        end
+        it 'is failed' do
+          expect(page).to have_content 'そのユーザー名は使用されています'
         end
       end
     end
