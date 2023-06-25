debbb5f97c63c14b4e73771fa4b064b51a168bac
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-06-25 21:52:44 +0900

    Add: 子ども新規登録のテストを作成しました。

--- spec/system/children_spec.rb
+++ spec/system/children_spec.rb
@@ -3,26 +3,47 @@ require 'rails_helper'
 RSpec.describe "Children", type: :system do
   let!(:user){ create(:user) }
   describe 'create' do
-    let(:child){ build(:child) }
-    before do
-      login(user)
-      click_on 'お子様の情報を登録'
-
-      fill_in 'お子様の名前', with: child.name
-      fill_in 'ステージ', with: child.stage
-      click_button '登録'
-    end
-    fcontext 'with all atttributes' do
+    let(:child){ build(:child, user: user) }
+    context 'with all atttributes' do
+      before do
+        login(user)
+        click_on 'お子様の情報を登録'
+  
+        fill_in 'お子様の名前', with: child.name
+        select '出産前', from: 'ステージ'
+        click_button '登録する'
+      end
       it 'is successful' do
-        fail
+        expect(current_path).to eq mypage_path
+        expect(page).to have_content 'お子様の情報を新規登録しました'
       end
     end
     context 'without name' do
+      before do
+        login(user)
+        click_on 'お子様の情報を登録'
+  
+        # 名前を入力しない
+        # fill_in 'お子様の名前', with: child.name
+        select '出産前', from: 'ステージ'
+        click_button '登録する'
+      end
       it 'is failed' do
+        expect(page).to have_content 'お子様の名前を入力してください'
       end
     end
     context 'without age' do
+      before do
+        login(user)
+        click_on 'お子様の情報を登録'
+  
+        fill_in 'お子様の名前', with: child.name
+        # ステージを選択しない
+        # select '出産前', from: 'ステージ'
+        click_button '登録する'
+      end
       it 'is failed' do
+        expect(page).to have_content 'ステージを入力してください'
       end
     end
   end
