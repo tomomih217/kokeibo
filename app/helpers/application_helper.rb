73e3943bf0b84d4d968599909c5ed7094635b631
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-07-03 00:58:54 +0900

    Add: シュミレーションの仕送り金額選択を作成しました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     options.push('出産前')
     options += (0..18).to_a.map{ |x| x.to_s + '才' }
   end
+
+  def living_alone_funds_options
+    options = []
+    options.push('選択してください')
+    options.push('仕送りの予定はない')
+    options += (1..20).map{ |x| x.to_s + '0,000円' }
+  end
 end
