c39990b7681e426513353589f41dc11b2a2532ab
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-07-05 23:51:59 +0900

    Add: デコレーターを適用するために、active_decoratorというgemもインストールしました。

---
+++ app/decorators/result_decorator.rb
@@ -0,0 +1,17 @@
+class ResultDecorator < ApplicationDecorator
+  delegate_all
+
+  def duration
+    (18 - object.age) * 12
+  end
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
