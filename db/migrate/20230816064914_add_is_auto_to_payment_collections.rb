059650c86af2772ceffd49de1d952abea0925b07
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-08-16 15:52:11 +0900

    Add: PaymentCollectionモデルにis_autoカラムを追加しました。

--- db/migrate/20230816064914_add_is_auto_to_payment_collections.rb
+++ db/migrate/20230816064914_add_is_auto_to_payment_collections.rb
@@ -1,5 +1,5 @@
 class AddIsAutoToPaymentCollections < ActiveRecord::Migration[6.1]
   def change
-    add_column :payment_collections, :is_auto, :boolean
+    add_column :payment_collections, :is_auto, :boolean, default: false
   end
 end
