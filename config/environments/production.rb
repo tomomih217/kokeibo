70a9ef6d988971f99c2e70e82d6fbe9a988316ee
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-08-30 04:03:59 +0900

    Add: アクションメイラーの設定を変更し、letter_openerで送信できることを確認しました。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kokeibo_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["ACTION_MAILER_USER"],
+    password:             ENV["ACTION_MAILER_KEY"],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
