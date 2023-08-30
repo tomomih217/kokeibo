d6640787ec2235ba4a7fbbca8cb8a8d51fcde4dd
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-08-31 01:10:51 +0900

    Fix: お問い合わせフォームを日本語に翻訳しました。

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :contact do
-    email { info@example.com }
+    email { 'info@example.com' }
     content { 'これはお問い合わせのテストです' }
-    
+
     user
   end
 end
--- spec/system/contacts_spec.rb
+++ spec/system/contacts_spec.rb
@@ -4,13 +4,35 @@ RSpec.describe 'Contact', type: :system do
   let!(:user){ create(:user) }
   let!(:child){ create(:child, user: user) }
   describe 'create' do
+    let(:contact){ build(:contact, user: user) }
+    before do
+      login(user)
+      click_on 'お問い合わせ'
+    end
     context 'access' do
-      before do
-        login(user)
-        click_on 'お問い合わせ'
-      end
       it 'is successful' do
         expect(current_path).to eq new_contact_path
+      end
+    end
+    fcontext 'with all attributes' do
+      before do
+        fill_in 'メールアドレス', with: contact.email
+        fill_in 'お問い合わせ内容', with: contact.content
+
+        click_button '送信'
+      end
+      it 'is successful' do
+        expect(page).to have_content 'お問い合わせを送信しました。ありがとうございました。'
+        expect(current_path).to eq mypage_path
+      end
+    end
+    context 'without email' do
+      it 'is failed' do
+        
+      end
+    end
+    context 'without content' do
+      it 'is failed' do
       end
     end
   end
